c48dd637864dbb8108aca11bfbf825d2f6be2abc
kgriswo1 <kgriswo1@binghamton.edu>
2019-10-23 15:46:04 -0400

    added active for header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,24 +2,50 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
+    
+    state = {
+        home: false,
+        plants: false, 
+        notes: false,
+        login: true
+    }
+
+    changeActive = (e) => {
+        if (this.props.user.id) {
+            this.setState({
+                home: false,
+                plants: false, 
+                notes: false,
+            })
+            this.setState({
+                [e.target.name]: true
+            })
+        } else {
+            this.setState({
+                login: true
+            })
+        }
+    }
+
     render() {
+        console.log(this.state.home)
         return (
             <div className="ui huge green secondary pointing menu fixed sticky">
-                <Link to="/" className="item">
+                <Link to="/" name="home" className={this.state.home ? "active item" : "item"} onClick={(e) => this.changeActive(e)}>
                     Home
                 </Link>
-                <Link to="/plants" className="item">
+                <Link to="/plants" name="plants" className={this.state.plants ? "active item" : "item"} onClick={(e) => this.changeActive(e)}>
                     Plants
                 </Link>
-                <Link to="/notes" className="item">
+                <Link to="/notes" name="notes" className={this.state.notes ? "active item" : "item"} onClick={(e) => this.changeActive(e)}>
                     Notes
                 </Link>
                 <div className="right menu">
                     {this.props.user.id
                     ?
-                    <a className="item" onClick={this.props.logout}>Logout</a>
+                    <a className="item" onClick={(e) => {this.changeActive(e); this.props.logout()}}>Logout</a>
                     :
-                    <Link to="/login" className="item">Login</Link>}
+                    <Link to="/login" className={this.state.login ? "active item" : "item"} onClick={(e) => this.changeActive(e)}>Login</Link>}
                 </div>
             </div>
         )
