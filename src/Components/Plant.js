1e636d353f9e4907910a330d37057ad8f11fa130
aresnik11 <aresnik11@gmail.com>
2019-11-23 14:25:41 -0500

    changed plant to functional component

--- src/Components/Plant.js
+++ src/Components/Plant.js
@@ -1,38 +1,36 @@
 import React from 'react'
 
-class Plant extends React.Component {
-    render() {
-        return (
-            // plant card including image, name, species, icons with info for light, water, and color, and delete button
-            <div className="ui green card plant-info">
-                <div className="image">
-                    <img alt={this.props.plant.name} src={this.props.plant.image}/>
+const Plant = (props) => {
+    return (
+        // plant card including image, name, species, icons with info for light, water, and color, and delete button
+        <div className="ui green card plant-info">
+            <div className="image">
+                <img alt={props.plant.name} src={props.plant.image}/>
+            </div>
+            <div className="content">
+                <div className="header">
+                    <h1>{props.plant.name}</h1>
                 </div>
-                <div className="content">
-                    <div className="header">
-                        <h1>{this.props.plant.name}</h1>
-                    </div>
-                    <div className="description">
-                        {this.props.plant.species}
-                    </div>
-                </div>
-                <div className="content">
-                    <i className="tint green large icon"></i>
-                    {this.props.plant.water}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <i className="sun green large icon"></i>
-                    {this.props.plant.light}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <i className="paint brush green large icon"></i>
-                    {this.props.plant.color}
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                </div>
-                <div className="content">
-                    <button className="ui green basic button" onClick={() => this.props.deletePlant(this.props.plant)}>Delete Plant</button>
+                <div className="description">
+                    {props.plant.species}
                 </div>
             </div>
-        )
-    }
+            <div className="content">
+                <i className="tint green large icon"></i>
+                {props.plant.water}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <i className="sun green large icon"></i>
+                {props.plant.light}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <i className="paint brush green large icon"></i>
+                {props.plant.color}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+            <div className="content">
+                <button className="ui green basic button" onClick={() => props.deletePlant(props.plant)}>Delete Plant</button>
+            </div>
+        </div>
+    )
 }
 
 export default Plant
