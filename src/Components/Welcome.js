9e81b4f5bdb1e8cf2d7dbdf06f7f658c7758a255
aresnik11 <aresnik11@gmail.com>
2019-11-23 13:14:45 -0500

    added buttons to welcome page

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Welcome = (props) => {
     return (
         <div className="welcome">
             <h1>Welcome {props.user.name}</h1>
+            <br/>
+            <Link to="/plants">
+                <button className="ui green basic button">My Plants</button>
+            </Link>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/notes">
+                <button className="ui green basic button">My Notes</button>
+            </Link>
+
             {/* css plant image from https://codepen.io/daliannyvieira/pen/pbJZMQ */}
             <div className="background">
                 <div className="table">
